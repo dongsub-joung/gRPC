5a427fd8c6bd4b601e0c26b3bd6b5117f934cc96
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2022-04-23 19:55:43 +0900

    bulid

---
+++ grpc-demo-tonic/build.rs
@@ -0,0 +1,12 @@
+fn main(){
+    let proto_file= "./proto/hello.proto";
+
+    tonic_build::configure()
+        .build_server(true)
+        .compile(&[proto_file], &["."])
+        .unwrap_or_else(|e| {
+            panic!("protobuf compile error: {}", e);
+        });
+
+        println!("cargo:return-if-changed= {}", proto_file);
+}
